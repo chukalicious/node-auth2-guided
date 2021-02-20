f0269e52b0f8e362a9af4f00c4a2274e9a61a175
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-20 15:49:17 -0800

    progress up to the use and verification of the token

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,4 +1,27 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../config/secrets");
+
 module.exports = (req, res, next) => {
   // add code here to verify users are logged in
-  next();
+
+  //get the token out of the request from the header
+  //Authorization: Bearer <token>
+  //an option to declare what's on line: 12
+  const token = req.headers?.authorization?.split(" ")[1];
+
+  //const token =
+  //req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  if (token) {
+    jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ you: "can't touch this" });
+      } else {
+        req.decodedJWT = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass" });
+  }
 };
