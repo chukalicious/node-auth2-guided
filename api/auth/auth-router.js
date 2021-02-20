e453fcd09a5b789ba4f01dc354334edcea74ac23
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-20 14:57:44 -0800

    progress up to the creation of the token

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
 const bcryptjs = require("bcryptjs");
 
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+const server = require("../server");
 
 const Users = require("../users/users-model.js");
 const { isValid } = require("../users/users-service.js");
@@ -18,15 +21,16 @@ router.post("/register", (req, res) => {
 
     // save the user to the database
     Users.add(credentials)
-      .then(user => {
+      .then((user) => {
         res.status(201).json({ data: user });
       })
-      .catch(error => {
+      .catch((error) => {
         res.status(500).json({ message: error.message });
       });
   } else {
     res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
+      message:
+        "please provide username and password and the password should be alphanumeric",
     });
   }
 });
@@ -39,19 +43,47 @@ router.post("/login", (req, res) => {
       .then(([user]) => {
         // compare the password the hash stored in the database
         if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
+          //
+          //
+          //after we have a user anc cheked their credentials, we're
+          //going to create a token by passing using the generateToken
+          //func and passing the user into it
+
+          const token = generateToken(user);
+          //
+          //on the response bellow we are sending the token
+          res.status(200).json({ message: `Welcome ${user.username}`, token });
         } else {
           res.status(401).json({ message: "Invalid credentials" });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         res.status(500).json({ message: error.message });
       });
   } else {
     res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
     });
   }
 });
+
+function generateToken(user) {
+  //the three things we need to create a token:
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+  };
+
+  const secret = secrets.jwtSecret;
+
+  const options = {
+    expiresIn: "1h",
+  };
+
+  //after all three parts have been created, return...
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
